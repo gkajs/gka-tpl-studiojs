72d5c924c7e6175e148a8e1b0812a303b7d7d365
joeyguo <lgsincy@gmail.com>
2017-08-03 15:38:44 +0800

    :sunglasses:

---
+++ index.js
@@ -0,0 +1,72 @@
+var html = require("./lib/html");
+
+function isSame(frames, key) {
+    var sameSourceWarr = frames.filter(function(f){
+        return f[key] === frames[0][key];
+    });
+    return (sameSourceWarr.length === frames.length? true: false);
+}
+
+module.exports = function (data, opts, tool) {
+    var prefix = opts.prefix,
+        frameduration = opts.frameduration * 1000;
+
+    var frames = data.frames;
+
+    var isSameSourceW = isSame(frames, "sourceW");
+    var isSameSourceH = isSame(frames, "sourceH");
+    var isSameWidth = isSame(frames, "width");
+    var isSameHeight = isSame(frames, "height");
+    
+    console.log(isSameSourceW,isSameSourceH)
+
+    isSameWidth && (data["width"] = frames[0].width);
+    isSameHeight && (data["height"] = frames[0].height);
+    isSameSourceW && (data["sourceW"] = frames[0].sourceW);
+    isSameSourceH && (data["sourceH"] = frames[0].sourceH);
+    
+    data.frames = frames.map(function(frame){
+        var res = {
+            "x": frame.x,
+            "y": frame.y,
+            "offX": frame.offX,
+            "offY": frame.offY,
+        };
+
+        !isSameWidth && (res["width"] = frame.width);
+        !isSameHeight && (res["height"] = frame.height);
+        !isSameSourceW && (res["sourceW"] = frame.sourceW);
+        !isSameSourceH && (res["sourceW"] = frame.sourceH);
+
+        return res;
+    });
+
+    var data2 = {};
+    data2.file = data.file;
+    data2.w = data.w;
+    data2.h = data.h;
+    data2.sourceW = data.sourceW;
+    data2.sourceH = data.sourceH;
+
+    data2.frames = frames.map(function(frame){
+        return [frame.x, frame.y, frame.width, frame.height, frame.offX, frame.offY]
+        // var res = {
+        //     "x": frame.x,
+        //     "y": frame.y,
+        //     "offX": frame.offX,
+        //     "offY": frame.offY,
+        // };
+
+        // !isSameWidth && (res["width"] = frame.width);
+        // !isSameHeight && (res["height"] = frame.height);
+        // !isSameSourceW && (res["sourceW"] = frame.sourceW);
+        // !isSameSourceH && (res["sourceW"] = frame.sourceH);
+
+        return res;
+    });
+
+    tool.writeFile("data.js", `var data = ${JSON.stringify(data, null, '    ')}`);
+    tool.writeFile("data2.js", `var data = ${JSON.stringify(data2, null, '    ')}`);
+    tool.writeFile("gka.html", html(data, prefix, frameduration));
+};
+
